de6b0e2f29375203046eee15b8f4f57fe35f2841
ohad2024 <133236955+ohad2024@users.noreply.github.com>
2024-05-10 01:54:34 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ohad-project
+# Recipe Finder Application
+
+## Description
+The Recipe Finder Application is a web-based application designed to help users discover recipes based on ingredients they have at home. It features a collection of dishes with detailed cooking instructions. 
+This application utilizes Flask for the backend, HTML and CSS for a simple yet effective user interface, and SQLite for database management.
+
+## Features
+- **Search Functionality**: Users can search for recipes based on specific ingredients.
+- **Detailed Views**: Each recipe has a detailed page with comprehensive cooking instructions.
+- **Database Storage**: The application stores user data in a local SQLite database.
+
+## Technologies Used
+- **Python**: The backend is entirely written in Python, using Flask as the web framework.
+- **Flask**: Serves the backend and handles routing and HTTP requests.
+- **HTML/CSS**: Used for creating the frontend of the application.
+- **SQLite**: Handles data storage, providing a lightweight database solution without the need for a separate server.
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dataclasses import dataclass
 import sqlite3
 
@@ -10,7 +10,6 @@ class Recipe:
     title: str
     ingredients: str
     instructions: str
-    
 
 def get_db_connection():
     conn = sqlite3.connect('recipes.db')
@@ -22,7 +21,7 @@ def index():
     conn = get_db_connection()
     recipes = conn.execute('SELECT * FROM recipes').fetchall()
     conn.close()
-    return render_template('index.html', recipes=recipes)
+    return render_template('index2.html', recipes=recipes)
 
 @app.route('/<int:recipe_id>')
 def recipe(recipe_id):
@@ -30,16 +29,6 @@ def recipe(recipe_id):
     recipe = conn.execute('SELECT * FROM recipes WHERE id = ?', (recipe_id,)).fetchone()
     conn.close()
     return render_template('recipe.html', recipe=recipe)
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        search_term = request.form['search']
-        conn = get_db_connection()
-        recipes = conn.execute('SELECT * FROM recipes WHERE ingredients LIKE ?', ('%' + search_term + '%',)).fetchall()
-        conn.close()
-        return render_template('search.html', recipes=recipes, search_term=search_term)
-    return render_template('search.html', recipes=[])
 
 if __name__ == '__main__':
     app.run(debug=True)
